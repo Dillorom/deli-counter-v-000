2c7b8e2ff0c0932b03f517b3a010dc4dcfccb86e
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-01 16:35:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli = []
+    puts "The line is currently empty."
+
+  elsif
+    phrase = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      phrase << " #{index}. #{name}"
+   end
+     phrase
+  end
+end
